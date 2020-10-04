997683ecaa206c80670b7e045a0c2f36da24a1cd
Mengyu LIU <me@mengyu.dev>
2020-10-04 12:56:18 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'anywhere-spinner-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'AnywhereSpinner';
+}
